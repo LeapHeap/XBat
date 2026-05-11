2fce997ffa5ffa184747d132c8eb58b621ac66e9
LeapHeap <leapheap027@gmail.com>
2026-05-11 11:28:44 +0800

    Optimized stub for vc98

--- src/stub/main.c
+++ src/stub/main.c
@@ -1,8 +1,11 @@
+#ifndef MODE_VC6
+#ifndef MODE_FULL
+#define MODE_FULL
+#endif
 #ifndef UNICODE
 #define UNICODE
-#endif
-#ifndef _UNICODE
 #define _UNICODE
+#endif
 #endif
 
 #include <windows.h>
@@ -59,6 +62,13 @@ char* WCharToAnsi(const WCHAR* wideStr) {
 	return multiStr;
 }
 
+static void ShowErrorMessage(LPCTSTR lpszMsg) {
+	DWORD dwErr = GetLastError();
+	TCHAR szBuf[256];
+	_sntprintf(szBuf, _countof(szBuf), _T("%s. Error: %u (0x%08X)"), lpszMsg, dwErr, dwErr);
+	szBuf[_countof(szBuf)-1] = _T('\0');
+	MessageBox(NULL, szBuf, _T("Stub Error"), MB_ICONERROR);
+}
 
 BOOL DirectoryExists(LPCTSTR szPath)
 {
@@ -97,7 +107,8 @@ BYTE* XBat_ExtractResource(const LPVOID pData, DWORD dwSize, const BYTE* pKey, i
 	if (Magic != *(UINT*)XBatMagic) return NULL;
 	
 	if (pszOutFileName){
-		_tcscpy(pszOutFileName, pHeader->szFileName);
+		_tcsncpy(pszOutFileName, pHeader->szFileName, XBAT_RES_FILE_NAME_LENGTH - 1);
+		pszOutFileName[XBAT_RES_FILE_NAME_LENGTH - 1] = _T('\0');
 	}
 	
 	if (pdwOutAttrib) {
@@ -135,12 +146,18 @@ BYTE* XBat_ExtractResourceEx(const LPVOID pData, DWORD dwSize, const BYTE* pKey,
 	XBAT_RES_HEADER* pHeader = (XBAT_RES_HEADER*)pData;
 	if (pHeader->Magic != *(UINT*)XBatMagic) return NULL;
 	
-	if (pszOutFileName) _tcscpy(pszOutFileName, pHeader->szFileName);
+	if (pszOutFileName){
+		_tcsncpy(pszOutFileName, pHeader->szFileName, XBAT_RES_FILE_NAME_LENGTH - 1);
+		pszOutFileName[XBAT_RES_FILE_NAME_LENGTH - 1] = _T('\0');
+	}
 	if (pdwOutAttrib) *pdwOutAttrib = pHeader->dwAttributes;
 	
 	UINT SavedCrc = pHeader->SavedCrc;
 	DWORD dwFinalSize = pHeader->dwOriginalSize;
-	DWORD dwCompressedSize = dwSize - (sizeof(XBAT_RES_HEADER) - 1); 
+	//DWORD dwCompressedSize = dwSize - (sizeof(XBAT_RES_HEADER) - 1); 
+	// Safer version
+	DWORD dwHeaderSize = (DWORD)((BYTE*)pHeader->Data - (BYTE*)pHeader);
+	DWORD dwCompressedSize = dwSize - dwHeaderSize;
 	
 	BYTE* pCompressed = (BYTE*)malloc(dwCompressedSize);
 	if (!pCompressed) return NULL;
@@ -172,58 +189,54 @@ void InitResPath(){
 	
 }
 
+//BOOL CALLBACK DbgEnumResNamesFunc(HMODULE hMod, LPCTSTR lpType, LPTSTR lpName, LONG_PTR lParam){
+//	MessageBox(NULL, _T("Inside Callback"), _T("Debug"), MB_OK);
+//	return TRUE;
+//}
+
+// Use static variables to avoid stack problem in callback function
+static TCHAR s_szFileName[MAX_PATH];
+static TCHAR s_szFullPath[MAX_PATH];
 
 BOOL CALLBACK EnumResNamesFunc(HMODULE hMod, LPCTSTR lpType, LPTSTR lpName, LONG_PTR lParam){
 	if (IS_INTRESOURCE(lpName)){
-		HRSRC hRes = FindResource(hMod,lpName,(LPCTSTR)RT_RCDATA);
-		DWORD dwSize = SizeofResource(hMod,hRes);
-		HGLOBAL hData = LoadResource(hMod,hRes);
+		HRSRC hRes = FindResource(hMod, lpName, RT_RCDATA);
+		if (!hRes) return TRUE;
+		
+		DWORD dwSize = SizeofResource(hMod, hRes);
+		HGLOBAL hData = LoadResource(hMod, hRes);
 		void* pData = LockResource(hData);
+		
 		DWORD dwContentOutLen = 0;
-		TCHAR szFileName[MAX_PATH];
 		DWORD dwFileAttrib = 0;
 		BYTE* pContent = NULL;
 		
+		// Clean old data
+		s_szFileName[0] = _T('\0');
+		
 #ifdef MODE_FULL
 		if (g_Config.GlobalFlags & XBAT_FLAG_LZMA_COMPRESSED){
-			pContent = XBat_ExtractResourceEx(pData,dwSize,FinalKey,XBAT_FINAL_KEY_LENGTH,&dwContentOutLen,szFileName,&dwFileAttrib);
+			pContent = XBat_ExtractResourceEx(pData, dwSize, FinalKey, XBAT_FINAL_KEY_LENGTH, &dwContentOutLen, s_szFileName, &dwFileAttrib);
 		} else {
-			pContent = XBat_ExtractResource(pData,dwSize,FinalKey,XBAT_FINAL_KEY_LENGTH,&dwContentOutLen,szFileName,&dwFileAttrib);
+			pContent = XBat_ExtractResource(pData, dwSize, FinalKey, XBAT_FINAL_KEY_LENGTH, &dwContentOutLen, s_szFileName, &dwFileAttrib);
 		}
 #else
-		pContent = XBat_ExtractResource(pData,dwSize,FinalKey,XBAT_FINAL_KEY_LENGTH,&dwContentOutLen,szFileName,&dwFileAttrib);
+		pContent = XBat_ExtractResource(pData, dwSize, FinalKey, XBAT_FINAL_KEY_LENGTH, &dwContentOutLen, s_szFileName, &dwFileAttrib);
 #endif
 		
-		if (pContent){
+		if (pContent && s_szFileName[0] != _T('\0')){
+			// Safe string printer
+			_sntprintf(s_szFullPath, MAX_PATH, _T("%s\\%s"), g_szSessionResPath, s_szFileName);
+			s_szFullPath[MAX_PATH - 1] = _T('\0');
 			
-			
-			TCHAR szFullPath[MAX_PATH];
-			_sntprintf(szFullPath, MAX_PATH, _T("%s\\%s"), g_szSessionResPath, szFileName);
-			
-			// Write file
-			HANDLE hFile = CreateFile(szFullPath, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+			HANDLE hFile = CreateFile(s_szFullPath, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 			if (hFile != INVALID_HANDLE_VALUE) {
 				DWORD dwWritten;
 				WriteFile(hFile, pContent, dwContentOutLen, &dwWritten, NULL);
 				CloseHandle(hFile);
-				
-				// Recover file attribute
-				SetFileAttributes(szFullPath, dwFileAttrib);
+				SetFileAttributes(s_szFullPath, dwFileAttrib);
 			}
-			
-			
 			free(pContent);
-		}
-		
-	}
-	
-	return TRUE;
-}
-
-BOOL CALLBACK EnumResTypesFunc(HMODULE hMod, LPTSTR lpType, LONG_PTR lParam){
-	if (IS_INTRESOURCE(lpType)){
-		if ((UINT_PTR)lpType == (UINT_PTR)RT_RCDATA){
-			EnumResourceNames(hMod,lpType,EnumResNamesFunc,0);
 		}
 	}
 	return TRUE;
@@ -272,39 +285,48 @@ void RunBatAsFile_Legacy_Internal(TCHAR* lpszFilePath, BOOL bShow) {
 }
 
 void ExecBat(BYTE* pBatContent, DWORD dwContentLen){
-	BOOL bShowConsole = (g_Config.GlobalFlags & XBAT_FLAG_SHOW_CONSOLE);
+//	TCHAR szDbg[128];
+//	wsprintf(szDbg, _T("Flags: 0x%08X, Pipe: %d, Full: %d"), 
+//			 g_Config.GlobalFlags, 
+//			 (g_Config.GlobalFlags & XBAT_FLAG_USE_PIPE) ? 1 : 0,
+//			 #ifdef MODE_FULL
+//			 1
+//			 #else
+//			 0
+//			 #endif
+//			 );
+//	MessageBox(NULL, szDbg, _T("Trace"), MB_OK);
 	
-	TCHAR szFilePath[MAX_PATH];
-	CHAR* pszFilePathA = NULL;
+	BOOL bShowConsole = (g_Config.GlobalFlags & XBAT_FLAG_SHOW_CONSOLE);
+	BOOL bUsePipe = FALSE;
+	BOOL bRunAsFile = TRUE; // Lite stub run bat as file by default
+	
 #ifdef MODE_FULL
-	if (bShowConsole) {
-		if (g_Config.GlobalFlags & XBAT_FLAG_USE_PIPE) SetupConsole(g_Config.szConsoleTitle);
-	}
+	bUsePipe = (g_Config.GlobalFlags & XBAT_FLAG_USE_PIPE);
+	bRunAsFile = (g_Config.GlobalFlags & XBAT_FLAG_RUN_BAT_AS_FILE);	
 #endif
 	
+	TCHAR szFilePath[MAX_PATH] = {0};
+	
+	// Init console
+	if (bShowConsole && bUsePipe) {
 #ifdef MODE_FULL
-	if (g_Config.GlobalFlags & XBAT_FLAG_USE_PIPE) {
-		if (g_Config.GlobalFlags & XBAT_FLAG_RUN_BAT_AS_FILE){
-			// Note: Fatih-like mode
-			if (!DropScriptToTemp(pBatContent, dwContentLen, szFilePath)){
-				DWORD dwErrCode = GetLastError();
-				TCHAR szBuf[256];
-				wsprintf(szBuf, _T("Failed to drop script, error code: %d (0x%08X)"), dwErrCode, dwErrCode);
-				MessageBox(NULL,szBuf,_T("Error"),MB_ICONERROR);
+		SetupConsole(g_Config.szConsoleTitle);
+#endif
+	}
+	
+#ifdef MODE_FULL
+	if (bUsePipe) {
+		if (bRunAsFile) {
+			// Fatih mode
+			if (!DropScriptToTemp(pBatContent, dwContentLen, szFilePath)) {
+				ShowErrorMessage(_T("DropScript(Pipe) Failed"));
 				return;
 			}
 #ifdef UNICODE
-			pszFilePathA = WCharToAnsi(szFilePath);
-			BOOL bRet = RunBatPipe(NULL, 0, bShowConsole, pszFilePathA);
-			if (!bRet) {
-				DWORD dwErrCode = GetLastError();
-				TCHAR szBuf[256];
-				wsprintf(szBuf, _T("Failed to run script in file-pipe mode, error code: %d (0x%08X)"), dwErrCode, dwErrCode);
-				MessageBox(NULL,szBuf,_T("Error"),MB_ICONERROR);
-				free(pszFilePathA);
-				return;
-			}
-			free(pszFilePathA);
+			char* pszPathA = WCharToAnsi(szFilePath);
+			RunBatPipe(NULL, 0, bShowConsole, pszPathA);
+			if (pszPathA) free(pszPathA);
 #else
 			RunBatPipe(NULL, 0, bShowConsole, szFilePath);
 #endif
@@ -312,17 +334,17 @@ void ExecBat(BYTE* pBatContent, DWORD dwContentLen){
 			// Full pipe mode
 			RunBatPipe((LPCSTR)pBatContent, dwContentLen, bShowConsole, NULL);
 		}
-		
-	} else {
-		// Otherwise: Full legacy
-		DropScriptToTemp(pBatContent, dwContentLen, szFilePath);
-		RunBatAsFile_Legacy_Internal(szFilePath, bShowConsole);
+		return; // Exite after pipe mode
 	}
-#else
-	DropScriptToTemp(pBatContent, dwContentLen, szFilePath);
-	RunBatAsFile_Legacy_Internal(szFilePath, bShowConsole);
 #endif
 	
+	// Legacy mode
+	// Fallback for lite mode and pipe not closed
+	if (DropScriptToTemp(pBatContent, dwContentLen, szFilePath)) {
+		RunBatAsFile_Legacy_Internal(szFilePath, bShowConsole);
+	} else {
+		ShowErrorMessage(_T("DropScript(Legacy) Failed"));
+	}
 }
 
 void StubProcess(){
@@ -394,9 +416,17 @@ void StubProcess(){
 #endif
 			free(pBatContent); // Free the og bat content memory
 			
-			EnumResourceTypes(hMod,EnumResTypesFunc,0); // Parse user-added resources
+//			// g_szSessionDropPath backup
+//			TCHAR szSessionDropPath[MAX_PATH];
+//			memcpy(szSessionDropPath, g_szSessionDropPath, sizeof(szSessionDropPath));
+			
+			EnumResourceNames(hMod, RT_RCDATA, EnumResNamesFunc, 0);
+			
+//			// Recover g_szSessionDropPath
+//			memcpy(g_szSessionDropPath, szSessionDropPath, sizeof(g_szSessionDropPath));
 		} 
 		
+		//MessageBox(NULL, _T("About to enter ExecBat"), _T("msg"), MB_OK);
 		ExecBat(pFinalContent, dwFinalLen);
 		
 		if (pFinalContent) free(pFinalContent);
@@ -471,11 +501,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	
 	StubProcess();
 	StubDestroy();
-	
-	
-#ifdef MODE_FULL
-	
-#endif	
 	
 	return 0;
 }
